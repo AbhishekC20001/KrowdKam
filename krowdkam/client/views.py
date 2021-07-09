b0296216e72ed7ceb3da1bdceb3a76ce6458c122
Abhishek Chopra <abhishek.chopra@spit.ac.in>
2021-07-09 20:52:08 +0530

    webcam integration

--- krowdkam/client/views.py
+++ krowdkam/client/views.py
@@ -1,3 +1,53 @@
+from django.apps import apps
+from django.http import HttpResponse
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
+from django.core.mail import EmailMessage
+from django.views.decorators import gzip
+from django.http import StreamingHttpResponse
+import cv2
+import threading
+
 
 # Create your views here.
+def clienthome(request):
+    return HttpResponse("Hello, world")
+
+
+@gzip.gzip_page
+def Home(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:
+        pass
+    return render(request, 'recording.html')
+
+
+# to capture video class
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n') 
