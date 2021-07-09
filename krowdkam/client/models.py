7f4d2b446302370d23ae1268ec973124d2fff397
Abhishek Chopra <abhishek.chopra@spit.ac.in>
2021-07-09 20:49:09 +0530

    created imp models

--- krowdkam/client/models.py
+++ krowdkam/client/models.py
@@ -1,3 +1,47 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
+
+
+def upload_cam_recordings(instance, filename):
+    return 'recording/recording_{0}/files/{1}'.format(instance.id, filename)
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=500, default='')
+    description = models.CharField(max_length=600)
+    address = models.CharField(max_length=1000, default='')
+    long = models.DecimalField(max_digits=9, decimal_places=6)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    status = models.IntegerField(default=1)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(default=now, editable=False)
+
+
+class Zone(models.Model):
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE,default=None,null=True, blank=True)
+    name = models.CharField(max_length=500, default='')
+    description = models.CharField(max_length=600)
+    location = models.CharField(max_length=1000, default='')
+    status = models.IntegerField(default=1)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(default=now, editable=False)
+
+
+class CCTVcam(models.Model):
+    zone = models.ForeignKey(Zone, on_delete=models.CASCADE,default=None,null=True, blank=True)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, default=None, null=True, blank=True)
+    recording = models.FileField(upload_to=upload_cam_recordings, null=True, blank=True)
+    position = models.CharField(max_length=1000, default='')
+    status = models.IntegerField(default=1)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(default=now, editable=False)
+
+
+class AnalysisReport(models.Model):
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, default=None, null=True, blank=True)
+    num_of_infringements = models.IntegerField(default=0)
+    status = models.IntegerField(default=1)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(default=now, editable=False)
+
--- krowdkam/guser/models.py
+++ krowdkam/guser/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.utils.timezone import now
 # Create your models here.
+
+
+class User(models.Model):
+    gender = models.CharField(max_length=10)
+    name = models.CharField(max_length=200,default='')
+    age = models.IntegerField(default=0)
+    mobile = models.CharField(max_length=20,default='')
+    email = models.CharField(max_length=500, default='')
+    country_code = models.CharField(default="+91", max_length=10)
+    status = models.IntegerField(default=1)
+    created_at = models.DateTimeField(default=now, editable=False)
+    updated_at = models.DateTimeField(default=now, editable=False)
